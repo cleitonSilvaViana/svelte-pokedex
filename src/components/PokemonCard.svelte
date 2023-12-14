692597a15cd4e4fd52dde1cb6e9daadbaf57c665
Cleiton Viana <cleitonviana.2@outlook.com.br>
2023-12-14 00:59:30 -0300

    Adicionado layout da seção contendo a lista de pokemons

--- src/components/PokemonCard.svelte
+++ src/components/PokemonCard.svelte
@@ -4,9 +4,52 @@
 </script>
 
 <article>
-
+    <h2>Bulbassaur</h2>
+    <div>
+        <ul>
+            <li>fire</li>
+            <li>Water</li>
+        </ul>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="...">
+    </div>
 </article>
 
 <style>
+    article {
+        width: 100%;
+        border-radius: .8rem;
+        background-color: aquamarine;
+        padding: 1.6rem .8rem .8rem;
+    }
 
+    h2 {
+        color: #333;
+        font-size: 2rem;
+    }
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        gap: .8rem;
+    }
+
+    ul {
+        display: inline-block;
+    }
+
+    li {
+        padding: .8rem 1.6rem;
+        border-radius: .8rem;
+        background-color: aqua;
+        width: fit-content;
+        margin: .8rem 0;
+        font-size: 1.4rem;
+        text-transform: capitalize;
+    }
+
+    img {
+        display: inline-block;
+        width: 50%;
+        max-width: 116px;
+    }
 </style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,5 +1,8 @@
 <!-- Home page -->
 <script>
+	// components
+	import Card from '$components/PokemonCard.svelte'
+
 	const { data } = $props();
 	const { pokemons } = data;
 
@@ -13,34 +16,73 @@
 	<section class="pokemons__container">
 		<input type="text" placeholder="Search your pokemon">
 		<div class="pokemons__list">
-
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
 		</div>
 	</section>
 </main>
 
 <style>
 	main {
-		min-height: calc(100dvh - 88px);
+		height: calc(100dvh - 88px);
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		gap: 24px;
 	}
 
 	section {
+		padding: 1.6rem;
 		border-radius: 4px;
 		background-color: #ccc;
+		overflow: hidden;
+	}
+
+	.pokemons__container {
+		overflow-y: scroll;
 	}
 
 	.pokemons__container input {
 		width: 100%;
 		padding: .8rem 1.6rem;
-		border-radius: .4px;
+		border-radius: .4rem;
+		margin-bottom: 1.6rem;
+	}
+
+	.pokemons__list {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+		gap: .8rem;
 	}
 
 
 	@media screen and (max-width: 799px) {
 		main {
-			grid-template-columns: repeat(auto-fit, minmax(204px, 1fr));
+			grid-template-columns: 1fr;
+			gap: 2.4rem;
 		}
 	}
 
